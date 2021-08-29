54fcde67ce9b2690d03989384ae60510279301ce
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2021-08-29 09:04:40 +0530

    Experiment with react router

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
@@ -10,12 +11,12 @@ class Header extends React.Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/blog" target="_blank"  rel="noreferrer"> Blog </Nav.Link>
-                            <Nav.Link href="/projects" target="_blank"  rel="noreferrer"> Projects </Nav.Link>
-                            <Nav.Link href="/research" target="_blank"  rel="noreferrer"> Research </Nav.Link>
-                            <Nav.Link href="/bucketlist"> Bucket-List </Nav.Link>
+                            <Nav.Link href="/blog" target="_blank" rel="noreferrer"> Blog </Nav.Link>
+                            <Nav.Link href="/projects" target="_blank" rel="noreferrer"> Projects </Nav.Link>
+                            <Nav.Link href="/research" target="_blank" rel="noreferrer"> Research </Nav.Link>
+                            <Nav.Link> <Link to='/bucketlist'> Bucket-List</Link> </Nav.Link>
                             {/* <Nav.Link href="/r esources"> Resources </Nav.Link> */}
-                            <Nav.Link href="/resume" target="_blank"  rel="noreferrer"> Resume </Nav.Link>
+                            <Nav.Link href="/resume" target="_blank" rel="noreferrer"> Resume </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
