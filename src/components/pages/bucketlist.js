9406931483d3907cacbe9f54fb6384ed9b7e947a
Vivek Kaushal <vivek.kaushal@outlook.com>
2021-12-20 00:21:27 +0530

    linked bucketlist to google sheet

--- src/components/pages/bucketlist.js
+++ src/components/pages/bucketlist.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import list from '../../static/bucketList.json'
-
+import config from "../../config"
 class BucketList extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            done: 0,
+            total: 0,
+         }
+      }
+
+    async componentDidMount() {
+        const state = await populateListState();
+        this.setState(state);
+    }
+
     render() {
+        const data = this.state.data;
         const pad = { padding: 15, fontSize: 20 };
-        let total = 0, done = 0;
-        list["items"].forEach((item) =>{ 
-            if (item.done)
-                done+=1;
-            total+=1;
-        })
         return (
             <Container>
                 {/* <h1>Bucket List</h1> */}
@@ -20,16 +28,15 @@ class BucketList extends React.Component {
                         I was inspired by Chip Hyuen's <a href="https://huyenchip.com/list-100/" target="_blank" rel="noreferrer"> List 100</a> to create and maintain this list.
                         There is no set parameter for what a 'successful' life is, it's a very personal, perspective-driven and subjective quantification. 
                         But we really do remember life in moments, so this list is an ambitious collections of moments that I want to experience before I drop dead. 
-                        The realization of your mortality makes life so much more exciting. This list is up to date, as of {list.lastUpdated}. <br/>
-                        Current status : {done} / {total}
+                        The realization of your mortality makes life so much more exciting. <br/>
+                        Current status : {this.state.done} / {this.state.total}
                     </Col>
                 </Row>
                 <Row style={pad}>
                     <Col>
                         <ol>
-                            {list["items"].map((item) => {
-                                const check = item.done ? '✅' : '⬜';
-                                return <li> {check} {item.body} </li>
+                            {data?.values?.map((value) => {
+                                return <li> {value[0]} {value[1]} </li>
                             })
                             }
                         </ol>
@@ -37,6 +44,24 @@ class BucketList extends React.Component {
                 </Row>
             </Container>
         );
+    }
+}
+
+async function populateListState() {
+    const data = await  fetch(config.sheetLink)
+    .then((response)=> {return response.json()});
+    let done = 0;
+    let total = 0;
+    data.values.forEach(ele => {
+        if (ele[1] === '✅') {
+            done++;
+        }
+        total++;
+    });
+    return {
+        data: data,
+        done: done,
+        total: total,
     }
 }
 
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config =  {
+    sheetLink : process.env.REACT_APP_SHEET_LINK
+}
+
+export default config;
