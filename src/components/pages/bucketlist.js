bf8cd4611aca0c062d8f85abd910d0e11f8d87ce
= <=>
2023-12-31 13:50:41 +0530

    message fixed

--- src/components/pages/bucketlist.js
+++ src/components/pages/bucketlist.js
@@ -30,9 +30,7 @@ class BucketList extends React.Component {
                 <Row md={8} xs={12} style={pad}>
                     <Col>
                     I was inspired by Chip Hyuen's <a href="https://huyenchip.com/list-100/" target="_blank" rel="noreferrer"> List 100</a> to create and maintain this list.
-                        There is no set parameter for what a 'successful' life is, it's a very personal, perspective-driven and subjective quantification. 
-                        But we really do remember life in moments, so this list is an ambitious collections of moments that I want to experience before I drop dead. 
-                        The realization of your mortality makes life so much more exciting. <br/>
+                        This list is a collections of moments that I want to experience before I drop off the face of this planet.<br/>
                         Current status : {done} / {total}
                     </Col>
                 </Row>
