5e2d423e4717994553502094792d00b30b47545d
Vivek Kaushal <vivek.kaushal@outlook.com>
2024-05-11 22:23:42 +0530

    bio update

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -11,13 +11,13 @@ class Home extends React.Component {
                 <Row className="align-items-center home-row">
                     <Col md={8} className="text-section">
                         <p>
-                        I am a <a href="https://en.wikipedia.org/wiki/Hacker" target="_black">hacker</a>. I am deeply curious about technology, and human behaviour. I graduated out of IIIT-H as a software engineer with a masters in cognitive neuroscience.
+                        I am a <a href="https://en.wikipedia.org/wiki/Hacker" target="_black">hacker</a>. Deeply curious about technology, and human behaviour, I graduated from IIIT-H as an engineer with a master's in cognitive neuroscience.
                             <br /><br />
-                            I've been a behavioral economics researcher at NTU, Taipei, a senior engineer at Samsung Research, engineered backend systems for a few early stage startups, and developed widely-used apps for the Delhi Government and Indian Railways.
+                            I've engineered backend systems at Samsung Research and a few early stage startups, developed widely-used apps for the Delhi Government and Indian Railways, and worked as a behavioral economics researcher at NTU, Taipei.
                             <br /><br />
-                            I started my <i>career</i> as a backend engineer, but the latter of my curiosities drove me towards empathizing with users, and I work as a Product Manager today at <a href="https://enterpret.com" target="_black">Enterpret</a>, building a state-of-art AI tool that helps the best product teams on the planet learn from their customers' feedback.
+                            I started my <i>career</i> as a backend engineer, but the latter of my curiosities drove me towards empathizing with users, and I work as a Product Manager today at <a href="https://enterpret.com" target="_black">Enterpret</a>, helping the best product teams on the planet learn from their customers' feedback.
                             <br /><br />
-                            I like coffee, non-fiction and indie rock music. I spend a lot of time hacking on AI agents and I challenge myself to go on adventures from time to time. I share what I learn on <a href="https://twitter.com/vi_kaushal" target="_blank" rel="noreferrer">X</a>, and on my blog <a href="https://vivekkaushal.substack.com" target="_blank" rel="noreferrer">Empathetic Hacking</a>.
+                            When I am not hacking on an idea, I challenge myself to go on adventures. I like coffee, non-fiction, and indie rock music. I share what I learn on <a href="https://twitter.com/vi_kaushal" target="_blank" rel="noreferrer">X</a>, and on my blog <a href="https://vivekkaushal.substack.com" target="_blank" rel="noreferrer">Empathetic Hacking</a>.
                             <br /><br />
                             Subscribe to stay in touch! ✨<br /><br />
                         </p>
